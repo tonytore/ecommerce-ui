6d34685f12cc1c3d9db74f7cabae057b54c434dc
tonytor <teme4842@gmail.com>
2025-10-22 08:42:34 +0300

    Refactor payments and users data structure, enhance UI components
    - Updated Payment type to include fullName and userId, replacing username.
    - Modified columns in payments table to reflect new user data structure.
    - Added delete functionality for selected payments in the DataTable component.
    - Created new columns and data structure for products and users, including image handling and action menus.
    - Implemented user management features with a detailed user page and edit functionality.
    - Enhanced UI components for better user experience and data presentation.

--- admin/src/components/EditUser.tsx
+++ admin/src/components/EditUser.tsx
@@ -29,25 +29,25 @@ import {
 import { Button } from "./ui/button";
 
 const formSchema = z.object({
-  username: z
+  fullName: z
     .string()
-    .min(2, { message: "Username must be at least 2 characters!" })
+    .min(2, { message: "fullName must be at least 2 characters!" })
     .max(50),
   email: z.string().email({ message: "Invalid email address!" }),
   phone: z.string().min(10).max(15),
-  location: z.string().min(2),
-  role: z.enum(["admin", "user"]),
+  address: z.string().min(2),
+  city: z.string().min(2)
 });
 
 const EditUser = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "john.doe",
+      fullName: "john Doe",
       email: "john.doe@gmail.com",
       phone: "+1 234 5678",
-      location: "New York, NY",
-      role: "admin",
+      address: "123 Main St",
+      city: "New York",
     },
   });
   return (
@@ -59,15 +59,15 @@ const EditUser = () => {
             <form className="space-y-8">
               <FormField
                 control={form.control}
-                name="username"
+                name="fullName"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Username</FormLabel>
+                    <FormLabel>Full Name</FormLabel>
                     <FormControl>
                       <Input {...field} />
                     </FormControl>
                     <FormDescription>
-                      This is your public username.
+                      Enter user Full Name
                     </FormDescription>
                     <FormMessage />
                   </FormItem>
@@ -107,39 +107,31 @@ const EditUser = () => {
               />
               <FormField
                 control={form.control}
-                name="location"
+                name="address"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Location</FormLabel>
+                    <FormLabel>Address</FormLabel>
                     <FormControl>
                       <Input {...field} />
                     </FormControl>
                     <FormDescription>
-                      This is the public location.
+                      Enter user address (optional).
                     </FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-              <FormField
+             <FormField
                 control={form.control}
-                name="role"
+                name="city"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Role</FormLabel>
+                    <FormLabel>City</FormLabel>
                     <FormControl>
-                      <Select>
-                        <SelectTrigger>
-                          <SelectValue placeholder="Role" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectItem value="admin">Admin</SelectItem>
-                          <SelectItem value="user">User</SelectItem>
-                        </SelectContent>
-                      </Select>
+                      <Input {...field} />
                     </FormControl>
                     <FormDescription>
-                      Only verified users can be admin.
+                      Enter user city (optional).
                     </FormDescription>
                     <FormMessage />
                   </FormItem>
